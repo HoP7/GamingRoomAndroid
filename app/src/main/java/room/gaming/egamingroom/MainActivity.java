5ba97cbb277e85465360f8c91432f1b6da671f07
Kenan Gutic <Kenan.Gutic@sticos.no>
2018-11-18 00:23:00 +0100

    android app

---
+++ app/src/main/java/room/gaming/egamingroom/MainActivity.java
@@ -0,0 +1,13 @@
+package room.gaming.egamingroom;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
