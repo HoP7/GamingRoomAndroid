0068a2445fd3c508f21703c3d7333fcc53fb2cb0
Kenan <kenan.gutic@edu.fit.ba>
2018-12-05 21:56:50 +0100

    navigatio menu

--- app/src/main/java/room/gaming/egamingroom/MainActivity.java
+++ app/src/main/java/room/gaming/egamingroom/MainActivity.java
@@ -1,10 +1,15 @@
 package room.gaming.egamingroom;
 
+
 import android.app.Activity;
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import room.gaming.egamingroom.fragments.PaymentsFragment;
 import room.gaming.egamingroom.fragments.ProfileFragment;
@@ -13,13 +18,57 @@ import room.gaming.egamingroom.helper.MyFragmentUtilities;
 
 public class MainActivity extends AppCompatActivity {
 
+    private DrawerLayout dl;
+    private ActionBarDrawerToggle t;
+    private NavigationView nv;
+    private Activity currentActivity;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        currentActivity = this;
+        dl = (DrawerLayout)findViewById(R.id.activity_main);
+        t = new ActionBarDrawerToggle(this, dl,R.string.Open, R.string.Close);
 
-        MyFragmentUtilities.OpenAsReplace(this,TransfersFragment.newInstance());
+        dl.addDrawerListener(t);
+        t.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        nv = (NavigationView)findViewById(R.id.nv);
+        nv.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+
+                switch(id)
+                {
+                    case R.id.navmenu_profil_id:{
+                        MyFragmentUtilities.OpenAsReplace(currentActivity,ProfileFragment.newInstance());
+break;
+                    }
+                    case R.id.navmenu_transfers_id: {
+                        MyFragmentUtilities.OpenAsReplace(currentActivity, TransfersFragment.newInstance());
+                    break;
+                    }
+                    case R.id.navmenu_transactions_id: {
+                        MyFragmentUtilities.OpenAsReplace(currentActivity, PaymentsFragment.newInstance());
+                    break;}
+                }
+return  true;
+            }
+
+        });
+
+
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
 
+        if(t.onOptionsItemSelected(item))
+            return true;
+
+        return super.onOptionsItemSelected(item);
+    }
 }
